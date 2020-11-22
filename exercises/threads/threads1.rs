d7c711ff9903544ffb636083c627d6be00838951
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-11-22 16:35:24 +0700

    thread always confusing

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,26 +5,26 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use std::sync::Mutex;
 
 struct JobStatus {
     jobs_completed: u32,
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
-    let status_shared = status.clone();
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed:0 }));
+    let status_2 = status.clone();
     thread::spawn(move || {
+        let mut status_shared = status.lock().unwrap();
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
             status_shared.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+    while status_2.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
